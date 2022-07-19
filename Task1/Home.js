e2e9fb9481c5fd536459e783dcb1ca0978d246f6
jaydeep <jaydeepmane4329@gmail.com>
2022-07-19 12:05:36 +0530

    initial commit

---
+++ Task1/Home.js
@@ -0,0 +1,8 @@
+let popUp = document.getElementById("dilouge");
+function playVideo() {
+  popUp.style.display = "block";
+}
+
+function closePopUp() {
+  popUp.style.display = "none";
+}
---
+++ Task1/list.js
@@ -0,0 +1,9 @@
+let dilog = document.getElementById("dilog");
+
+let showImage = () => {
+  dilog.style.display = "block";
+};
+
+let hideImage = () => {
+  dilog.style.display = "none";
+};
