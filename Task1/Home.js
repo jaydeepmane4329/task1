0cd2c2d7352c6231452cd8075bed1963502fa0c1
jaydeep <jaydeepmane4329@gmail.com>
2022-07-20 16:18:35 +0530

    initial commit

--- Task1/Home.js
+++ Task1/Home.js
@@ -1,8 +1,19 @@
+// opening video dilog box
 let popUp = document.getElementById("dilouge");
 function playVideo() {
   popUp.style.display = "block";
 }
 
+// Close video dilouge box
 function closePopUp() {
   popUp.style.display = "none";
+}
+
+// Conditional Rendering for Home and List Comonent
+function list() {
+  let listComponent = document.getElementById("listComponent");
+  listComponent.style.display = "!block" ? "block" : "none";
+
+  let homeComponent = document.getElementById("homeComponent");
+  homeComponent.style.display = "none";
 }
--- Task1/list.js
+++ Task1/list.js
@@ -1,5 +1,6 @@
-let dilog = document.getElementById("dilog");
+let parent = document.getElementById("data");
 
+let dilog = document.getElementById("dilog");
 let showImage = () => {
   dilog.style.display = "block";
 };
@@ -7,3 +8,53 @@ let showImage = () => {
 let hideImage = () => {
   dilog.style.display = "none";
 };
+
+// courousal
+document.querySelectorAll(".carousel").forEach((carousel) => {
+  const items = carousel.querySelectorAll(".carousel__item");
+  const buttonsHtml = Array.from(items, () => {
+    return `<span class="carousel__button"></span>`;
+  });
+
+  carousel.insertAdjacentHTML(
+    "beforeend",
+    `
+		<div class="carousel__nav">
+			${buttonsHtml.join("")}
+		</div>
+	`
+  );
+
+  const buttons = carousel.querySelectorAll(".carousel__button");
+
+  buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+      // un-select all the items
+      items.forEach((item) =>
+        item.classList.remove("carousel__item--selected")
+      );
+      buttons.forEach((button) =>
+        button.classList.remove("carousel__button--selected")
+      );
+
+      items[i].classList.add("carousel__item--selected");
+      button.classList.add("carousel__button--selected");
+    });
+  });
+
+  // Select the first item on page load
+  items[0].classList.add("carousel__item--selected");
+  buttons[0].classList.add("carousel__button--selected");
+});
+
+// To show popUp in full Screen
+document.querySelectorAll(".carousel img").forEach((image) => {
+  image.onclick = () => {
+    document.querySelector(".popUp_image").style.display = "block";
+    document.querySelector(".popUp_image img").src = image.src;
+  };
+
+  document.querySelector(".popUp_image span").onclick = () => {
+    document.querySelector(".popUp_image").style.display = "none";
+  };
+});
